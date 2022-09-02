a47d4cc2364310f577b72d2bf23027fd1e649ad0
奇葩の灵梦 <415551921@qq.com>
2022-09-03 06:52:20 +0800

    接收阶段使用技能收到协议没反应的bug

--- game/game.go
+++ game/game.go
@@ -207,17 +207,17 @@ func Start(totalCount int) {
 		case *protos.SkillMianLiCangZhenTos:
 			r := humanMap[ev.Session().ID()]
 			if skill := r.FindSkill(SkillIdMianLiCangZhen); skill != nil {
-				skill.ExecuteProtocol(r.game, r, pb)
+				r.GetGame().TryContinueResolveProtocol(r, pb)
 			}
 		case *protos.SkillQiHuoKeJuTos:
 			r := humanMap[ev.Session().ID()]
 			if skill := r.FindSkill(SkillIdQiHuoKeJu); skill != nil {
-				skill.ExecuteProtocol(r.game, r, pb)
+				r.GetGame().TryContinueResolveProtocol(r, pb)
 			}
 		case *protos.SkillJinShenTos:
 			r := humanMap[ev.Session().ID()]
 			if skill := r.FindSkill(SkillIdJinShen); skill != nil {
-				skill.ExecuteProtocol(r.game, r, pb)
+				r.GetGame().TryContinueResolveProtocol(r, pb)
 			}
 		}
 	})
