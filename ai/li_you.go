ed452f486aa2e84a276889559f43a815f9288e15
奇葩の灵梦 <415551921@qq.com>
2022-09-08 00:13:20 +0800

    优化利诱AI

--- ai/li_you.go
+++ ai/li_you.go
@@ -13,26 +13,11 @@ func init() {
 
 func liYou(e *game.MainPhaseIdle, card game.ICard) bool {
 	player := e.Player
-	var chooseHuman bool
-	n := utils.Random.Intn(3)
-	if n == 0 {
-		time.AfterFunc(2*time.Second, func() {
-			game.Post(func() { card.Execute(player.GetGame(), player, player) })
-		})
-		return true
-	} else {
-		chooseHuman = n == 1
-	}
 	var players []game.IPlayer
 	for _, p := range player.GetGame().GetPlayers() {
 		if p.IsAlive() {
-			if _, ok := p.(*game.HumanPlayer); ok == chooseHuman {
-				players = append(players, p)
-			}
+			players = append(players, p)
 		}
-	}
-	if len(players) == 0 {
-		players = player.GetGame().GetPlayers()
 	}
 	if len(players) == 0 {
 		return false
